d2434c6c6f717214607a845f89e1a4738287a6e2
EBrown8534 <ebrown8534@gmail.com>
2015-08-18 15:12:30 -0400

    Removed empty event handlers from `TextBox`.

--- Evbpc.Framework.Windows/Forms/TextBox.cs
+++ Evbpc.Framework.Windows/Forms/TextBox.cs
@@ -12,27 +12,7 @@ namespace Evbpc.Framework.Windows.Forms
     {
         public TextBox()
         {
-            KeyUp += TextBox_KeyUp;
-            KeyDown += TextBox_KeyDown;
             KeyPress += TextBox_KeyPress;
-        }
-
-        void TextBox_KeyDown(object sender, KeyEventArgs e)
-        {
-            //if (e.KeyCode == Keys.Back && this.Text.Length > 0)
-            //    this.Text = this.Text.Substring(0, this.Text.Length - 1);
-            //else if (e.KeyCode >= Keys.A && e.KeyCode <= Keys.Z)
-            //    if (e.Shift)
-            //        this.Text += e.KeyCode.ToString();
-            //    else
-            //        this.Text += e.KeyCode.ToString().ToLower();
-            //else if (e.KeyCode == Keys.Space)
-            //    this.Text += " ";
-        }
-
-        void TextBox_KeyUp(object sender, KeyEventArgs e)
-        {
-
         }
 
         void TextBox_KeyPress(object sender, KeyPressEventArgs e)
