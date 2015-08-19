49fbb742d84a9f290f62fc69489fb578183dd99d
EBrown8534 <ebrown8534@gmail.com>
2015-08-19 09:08:47 -0400

    Removed unnecessary `PositionChangedEventHandler` for `EventHandler<PositionChangedEventArgs>`.

--- Evbpc.Framework/Utilities/EventHandlerDelegates.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Evbpc.Framework.Utilities
-{
-    public delegate void PositionChangedEventHandler(Object sender, PositionChangedEventArgs e);
-}
