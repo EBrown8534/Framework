c2621d9db939923d2cc14a36c4cb0ceb78444b62
EBrown8534 <ebrown8534@gmail.com>
2015-08-25 14:36:19 -0400

    Cleaned up code for `Vector2` and `Vector2F` a bit.

--- Evbpc.Framework/Physics/Vector2.cs
+++ Evbpc.Framework/Physics/Vector2.cs
@@ -16,28 +16,36 @@ namespace Evbpc.Framework.Physics
         [BrowsableAttribute(false)]
         public bool IsEmpty { get { return this == Empty; } }
 
-        public int X { get { return _x; } set { _x = value; } }
-        public int Y { get { return _y; } set { _y = value; } }
+        public int X { get { return _x; } private set { _x = value; } }
+        public int Y { get { return _y; } private set { _y = value; } }
         public double R { get { return Math.Sqrt((double)_x * _x + (double)_y * _y); } }
         public double Theta { get { return Math.Atan2((double)_y, (double)_x); } }
 
         public static Vector2 Add(Vector2 v1, Vector2 v2) { return v1 + v2; }
         public static Vector2 Ceiling(Vector2F value) { return new Vector2((int)Math.Ceiling(value.X), (int)Math.Ceiling(value.Y)); }
         public override bool Equals(Object obj) { if (obj is Vector2) { return (Vector2)obj == this; } else { return false; } }
-        public override int GetHashCode() { return base.GetHashCode(); }
-        public void Offset(Vector2 p) { _x += p.X; _y += p.Y; }
-        public void Offset(int dx, int dy) { _x += dx; _y += dy; }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+
+            hash = hash * 23 + _x.GetHashCode();
+            hash = hash * 23 + _y.GetHashCode();
+
+            return hash;
+        }
+
         public static Vector2 Round(Vector2F value) { return new Vector2((int)Math.Round(value.X), (int)Math.Round(value.Y)); }
         public static Vector2 Subtract(Vector2 v1, Vector2 v2) { return v1 - v2; }
-        public override string ToString() { return "(" + _x + "," + _y + ")"; }
+        public override string ToString() { return string.Format("({0},{1})", _x, _y); }
         public static Vector2 Truncate(Vector2F value) { return new Vector2((int)(value.X), (int)(value.Y)); }
         public static Vector2 FromRTheta(double r, double theta) { return new Vector2((int)(r * Math.Cos(theta)), (int)(r * Math.Sin(theta))); }
 
-        public static Vector2 operator +(Vector2 v1, Vector2 v2) { return new Vector2(v1._x + v2._x, v1._y + v2._y); }
+        public static Vector2 operator +(Vector2 v1, Vector2 v2) { return new Vector2(v1.X + v2.X, v1.Y + v2.Y); }
         public static bool operator ==(Vector2 left, Vector2 right) { return left.X == right.X && left.Y == right.Y; }
-        public static implicit operator Vector2F(Vector2 p) { return new Vector2F(p._x, p._y); }
-        public static bool operator !=(Vector2 left, Vector2 right) { return left._x != right._x || left._y != right._y; }
-        public static Vector2 operator -(Vector2 v1, Vector2 v2) { return new Vector2(v1._x - v2._x, v1._y - v2._y); }
+        public static implicit operator Vector2F(Vector2 p) { return new Vector2F(p.X, p.Y); }
+        public static bool operator !=(Vector2 left, Vector2 right) { return left.X != right.X || left.Y != right.Y; }
+        public static Vector2 operator -(Vector2 v1, Vector2 v2) { return new Vector2(v1.X - v2.X, v1.Y - v2.Y); }
 
         public static readonly Vector2 Empty = new Vector2(0, 0);
     }
--- Evbpc.Framework/Physics/Vector2F.cs
+++ Evbpc.Framework/Physics/Vector2F.cs
@@ -16,27 +16,37 @@ namespace Evbpc.Framework.Physics
         [BrowsableAttribute(false)]
         public bool IsEmpty { get { return this == Empty; } }
 
-        public float X { get { return _x; } set { _x = value; } }
-        public float Y { get { return _y; } set { _y = value; } }
+        public float X { get { return _x; } private set { _x = value; } }
+        public float Y { get { return _y; } private set { _y = value; } }
         public double R { get { return Math.Sqrt((double)_x * _x + (double)_y * _y); } }
         public double Theta { get { return Math.Atan2((double)_y, (double)_x); } }
         
         public static Vector2F Add(Vector2F v1, Vector2 v2) { return v1 + v2; }
         public static Vector2F Add(Vector2F v1, Vector2F v2) { return v1 + v2; }
         public override bool Equals(Object obj) { if (obj is Vector2F) { return (Vector2F)obj == this; } else { return false; } }
-        public override int GetHashCode() { return base.GetHashCode(); }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+
+            hash = hash * 23 + _x.GetHashCode();
+            hash = hash * 23 + _y.GetHashCode();
+
+            return hash;
+        }
+
         public static Vector2F Subtract(Vector2F v1, Vector2 v2) { return v1 - v2; }
         public static Vector2F Subtract(Vector2F v1, Vector2F v2) { return v1 - v2; }
-        public override string ToString() { return "(" + _x + "," + _y + ")"; }
+        public override string ToString() { return string.Format("({0},{1})", _x, _y); }
         public static Vector2F FromRTheta(double r, double theta) { return new Vector2F((float)(r * Math.Cos(theta)), (float)(r * Math.Sin(theta))); }
         public Vector2F Invert() { return new Vector2F(0 - _x, 0 - _y); }
 
-        public static Vector2F operator +(Vector2F v1, Vector2 v2) { return new Vector2F(v1._x + v2.X, v1.Y + v2.Y); }
-        public static Vector2F operator +(Vector2F v1, Vector2F v2) { return new Vector2F(v1._x + v2._x, v1.Y + v2._y); }
+        public static Vector2F operator +(Vector2F v1, Vector2 v2) { return new Vector2F(v1.X + v2.X, v1.Y + v2.Y); }
+        public static Vector2F operator +(Vector2F v1, Vector2F v2) { return new Vector2F(v1.X + v2.X, v1.Y + v2.Y); }
         public static bool operator ==(Vector2F left, Vector2F right) { return left.X == right.X && left.Y == right.Y; }
         public static bool operator !=(Vector2F left, Vector2F right) { return left.X != right.X || left.Y != right.Y; }
-        public static Vector2F operator -(Vector2F v1, Vector2 v2) { return new Vector2F(v1._x - v2.X, v1._y - v2.Y); }
-        public static Vector2F operator -(Vector2F v1, Vector2F v2) { return new Vector2F(v1._x - v2._x, v1._y - v2._y); }
+        public static Vector2F operator -(Vector2F v1, Vector2 v2) { return new Vector2F(v1.X - v2.X, v1.Y - v2.Y); }
+        public static Vector2F operator -(Vector2F v1, Vector2F v2) { return new Vector2F(v1.X - v2.X, v1.Y - v2.Y); }
 
         public static readonly Vector2F Empty = new Vector2F(0, 0);
     }
