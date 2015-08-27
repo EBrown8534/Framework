d4edd014b7ba8b95dc47dd48d9da67af262438ca
EBrown8534 <ebrown8534@gmail.com>
2015-08-27 09:12:25 -0400

    Finished unit tests for `Evbpc.Framework.Drawing.Color`.

--- Evbpc.Framework.Tests/Drawing/ColorTests.cs
+++ Evbpc.Framework.Tests/Drawing/ColorTests.cs
@@ -27,48 +27,57 @@ namespace Evbpc.Framework.Tests.Drawing
         }
 
         [TestMethod, TestCategory("Color Tests")]
-        public void CreateColorStructure_R0x7F_G0x00_B0xAF_A0xFF()
+        public void CreateColorStructure_R0x7F_G0x00_B0xBF_A0xFF()
         {
-            Color color = new Color(0x7F, 0x00, 0xAF, 0xFF);
+            Color color = new Color(0x7F, 0x00, 0xBF, 0xFF);
 
-            Assert.AreEqual(0xFF7F00AF, color.GetPackedValue());
+            Assert.AreEqual(0xFF7F00BF, color.GetPackedValue());
         }
 
         [TestMethod, TestCategory("Color Tests")]
-        public void CreateColorStructure_Packed0xFF7F00AF()
+        public void CreateColorStructure_Packed0xFF7F00BF()
         {
-            Color color = new Color(0xFF7F00AF);
+            Color color = new Color(0xFF7F00BF);
 
             Assert.AreEqual(0xFF, color.A);
             Assert.AreEqual(0x7F, color.R);
             Assert.AreEqual(0x00, color.G);
-            Assert.AreEqual(0xAF, color.B);
+            Assert.AreEqual(0xBF, color.B);
         }
 
         [TestMethod, TestCategory("Color Tests")]
-        public void Equal_Packed0xFF7F00AF_Packed0xFF7F00AF()
+        public void Equal_Packed0xFF7F00BF_Packed0xFF7F00BF()
         {
-            Color color1 = new Color(0xFF7F00AF);
-            Color color2 = new Color(0xFF7F00AF);
+            Color color1 = new Color(0xFF7F00BF);
+            Color color2 = new Color(0xFF7F00BF);
 
             Assert.AreEqual(color1 == color2, true);
         }
 
         [TestMethod, TestCategory("Color Tests")]
-        public void NotEqual_Packed0xFF7F00AF_Packed0xFF7FAF00()
+        public void NotEqual_Packed0xFF7F00BF_Packed0xFF7FBF00()
         {
-            Color color1 = new Color(0xFF7F00AF);
-            Color color2 = new Color(0xFF7FAF00);
+            Color color1 = new Color(0xFF7F00BF);
+            Color color2 = new Color(0xFF7FBF00);
 
             Assert.AreEqual(color1 != color2, true);
         }
 
         [TestMethod, TestCategory("Color Tests")]
-        public void GetBytes_0xFF7F00AF()
+        public void GetBytes_0xFF7F00BF()
         {
-            Color color = new Color(0xFF7F00AF);
+            Color color = new Color(0xFF7F00BF);
 
-            CollectionAssert.AreEqual(color.GetBytes(), new byte[] { 0xFF, 0x7F, 0x00, 0xAF });
+            CollectionAssert.AreEqual(color.GetBytes(), new byte[] { 0xFF, 0x7F, 0x00, 0xBF });
+        }
+
+        [TestMethod, TestCategory("Color Tests")]
+        public void FromBytes_0xFF_0x7F_0x00_0xBF()
+        {
+            Color color = new Color();
+            color.FromBytes(new byte[] { 0xFF, 0x7F, 0x00, 0xBF });
+
+            Assert.AreEqual(color.GetPackedValue(), 0xFF7F00BFu);
         }
     }
 }
