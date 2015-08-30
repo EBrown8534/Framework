e30f92ad7fdd75341e62e2dd987ec907dd0167cd
Elliott Brown <ebrown8534@gmail.com>
2015-08-30 08:10:38 -0400

    Made `Evbpc.Framework.Drawing.Point` immutable.

--- Evbpc.Framework/Drawing/Point.cs
+++ Evbpc.Framework/Drawing/Point.cs
@@ -8,17 +8,17 @@ using System.Text;
 
 namespace Evbpc.Framework.Drawing
 {
-    [SerializableAttribute]
-    [TypeConverterAttribute(typeof(PointConverter))]
+    [Serializable]
+    [TypeConverter(typeof(PointConverter))]
     [StructLayout(LayoutKind.Explicit)]
     public struct Point : ISerializableByteArray
     {
         [FieldOffset(0)]
-        private ulong _packedValue;
+        private readonly ulong _packedValue;
         [FieldOffset(0)]
-        private int _x;
+        private readonly int _x;
         [FieldOffset(4)]
-        private int _y;
+        private readonly int _y;
 
         public Point(Size sz)
             : this()
@@ -34,31 +34,104 @@ namespace Evbpc.Framework.Drawing
             _y = y;
         }
 
-        [BrowsableAttribute(false)]
+        [Browsable(false)]
         public bool IsEmpty { get { return this == Empty; } }
 
-        public int X { get { return _x; } set { _x = value; } }
-        public int Y { get { return _y; } set { _y = value; } }
+        public int X { get { return _x; } }
+        public int Y { get { return _y; } }
 
         public int SizeInBytes { get { return 8; } }
 
-        public static Point Add(Point pt, Size sz) { return pt + sz; }
-        public static Point Ceiling(PointF value) { return new Point((int)Math.Ceiling(value.X), (int)Math.Ceiling(value.Y)); }
-        public override bool Equals(Object obj) { if (obj.GetType() == typeof(Point)) { return (Point)obj == this; } else { return false; } }
-        public override int GetHashCode() { return base.GetHashCode(); }
-        public void Offset(Point p) { _x += p.X; _y += p.Y; }
-        public void Offset(int dx, int dy) { _x += dx; _y += dy; }
-        public static Point Round(PointF value) { return new Point((int)Math.Round(value.X), (int)Math.Round(value.Y)); }
-        public static Point Subtract(Point pt, Size sz) { return pt - sz; }
-        public override string ToString() { return "(" + _x + "," + _y + ")"; }
-        public static Point Truncate(PointF value) { return new Point((int)(value.X), (int)(value.Y)); }
+        public static Point Add(Point pt, Size sz)
+        {
+            return pt + sz;
+        }
 
-        public static Point operator +(Point pt, Size sz) { return new Point(pt.X + sz.Width, pt.Y + sz.Height); }
-        public static bool operator ==(Point left, Point right) { return left.X == right.X && left.Y == right.Y; }
-        public static explicit operator Size(Point p) { return new Size(p.X, p.Y); }
-        public static implicit operator PointF(Point p) { return new PointF(p.X, p.Y); }
-        public static bool operator !=(Point left, Point right) { return left.X != right.X || left.Y != right.Y; }
-        public static Point operator -(Point pt, Size sz) { return new Point(pt.X - sz.Width, pt.Y - sz.Height); }
+        public static Point Ceiling(PointF value)
+        {
+            return new Point((int)Math.Ceiling(value.X), (int)Math.Ceiling(value.Y));
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj is Point)
+                return (Point)obj == this;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+
+            unchecked
+            {
+                hash = hash * 23 + _x.GetHashCode();
+                hash = hash * 23 + _y.GetHashCode();
+            }
+
+            return hash;
+        }
+
+        public void Offset(Point p)
+        {
+            this = new Point(_x + p.X, _y + p.Y);
+        }
+
+        public void Offset(int dx, int dy)
+        {
+            this = new Point(_x + dx, _y + dy);
+        }
+
+        public static Point Round(PointF value)
+        {
+            return new Point((int)Math.Round(value.X), (int)Math.Round(value.Y));
+        }
+
+        public static Point Subtract(Point pt, Size sz)
+        {
+            return pt - sz;
+        }
+
+        public override string ToString()
+        {
+            return $"({_x},{_y})";
+        }
+
+        public static Point Truncate(PointF value)
+        {
+            return new Point((int)(value.X), (int)(value.Y));
+        }
+        
+        public static Point operator +(Point pt, Size sz)
+        {
+            return new Point(pt.X + sz.Width, pt.Y + sz.Height);
+        }
+
+        public static bool operator ==(Point left, Point right)
+        {
+            return left.X == right.X && left.Y == right.Y;
+        }
+
+        public static explicit operator Size(Point p)
+        {
+            return new Size(p.X, p.Y);
+        }
+
+        public static implicit operator PointF(Point p)
+        {
+            return new PointF(p.X, p.Y);
+        }
+
+        public static bool operator !=(Point left, Point right)
+        {
+            return left.X != right.X || left.Y != right.Y;
+        }
+
+        public static Point operator -(Point pt, Size sz)
+        {
+            return new Point(pt.X - sz.Width, pt.Y - sz.Height);
+        }
 
         public static readonly Point Empty = new Point(0, 0);
 
@@ -81,10 +154,11 @@ namespace Evbpc.Framework.Drawing
         public void FromBytes(byte[] data)
         {
             if (data.Length != SizeInBytes)
-                throw new ArgumentException("The parameter `data` must be a " + SizeInBytes + " index byte-array.");
+                throw new ArgumentException($"The parameter \"data\" must be a {SizeInBytes} index byte-array.");
 
-            _x = ((int)data[0] << 24 | (int)data[1] << 16 | (int)data[2] << 8 | (int)data[3] << 0);
-            _y = ((int)data[4] << 24 | (int)data[5] << 16 | (int)data[6] << 8 | (int)data[7] << 0);
+            this = new Point(
+                ((int)data[0] << 24 | (int)data[1] << 16 | (int)data[2] << 8 | (int)data[3] << 0),
+                ((int)data[4] << 24 | (int)data[5] << 16 | (int)data[6] << 8 | (int)data[7] << 0));
         }
     }
 }
