e450d06f27c9632f96ca3baaa91453b28c9f4b74
Elliott Brown <ebrown8534@gmail.com>
2015-08-30 08:41:33 -0400

    Made `Evbpc.Framework.Drawing.Size` immutable.

--- Evbpc.Framework/Drawing/Size.cs
+++ Evbpc.Framework/Drawing/Size.cs
@@ -6,39 +6,96 @@ using System.Text;
 
 namespace Evbpc.Framework.Drawing
 {
-    [SerializableAttribute]
-    [TypeConverterAttribute(typeof(SizeConverter))]
+    [Serializable]
+    [TypeConverter(typeof(SizeConverter))]
     public struct Size
     {
-        private int _width;
-        private int _height;
+        private readonly int _width;
+        private readonly int _height;
 
         public Size(Point pt) { _width = pt.X; _height = pt.Y; }
         public Size(int width, int height) { _width = width; _height = height; }
 
-        public int Height { get { return _height; } set { _height = value; } }
+        public int Height { get { return _height; } set { this = new Size(_width, value); } }
 
         [BrowsableAttribute(false)]
         public bool IsEmpty { get { return this == Empty; } }
 
-        public int Width { get { return _width; } set { _width = value; } }
+        public int Width { get { return _width; } set { this = new Size(value, _height); } }
 
-        public static Size Add(Size sz1, Size sz2) { return sz1 + sz2; }
-        public static Size Ceiling(SizeF value) { return new Size((int)Math.Ceiling(value.Width), (int)Math.Ceiling(value.Height)); }
-        public override bool Equals(Object obj) { if (obj.GetType() == typeof(Size)) { return (Size)obj == this; } else { return false; } }
-        public override int GetHashCode() { return base.GetHashCode(); }
-        public static Size Round(SizeF value) { return new Size((int)Math.Round(value.Width), (int)Math.Round(value.Height)); }
-        public static Size Subtract(Size sz1, Size sz2) { return sz1 - sz2; }
-        public override string ToString() { return "(" + _width + "," + _height + ")"; }
-        public static Size Truncate(SizeF value) { return new Size((int)(value.Width), (int)(value.Height)); }
+        public static Size Add(Size sz1, Size sz2)
+        {
+            return sz1 + sz2;
+        }
 
-        public static Size operator +(Size sz1, Size sz2) { return new Size(sz1.Width + sz2.Width, sz1.Height + sz2.Height); }
-        public static bool operator ==(Size sz1, Size sz2) { return sz1.Width == sz2.Width && sz1.Height == sz2.Height; }
-        public static explicit operator Point(Size size) { return new Point(size.Width, size.Height); }
-        public static implicit operator SizeF(Size p) { return new SizeF(p.Width, p.Height); }
-        public static bool operator !=(Size sz1, Size sz2) { return sz1.Width != sz2.Width || sz1.Height != sz2.Height; }
-        public static Size operator -(Size sz1, Size sz2) { return new Size(sz1.Width - sz2.Width, sz1.Height - sz2.Height); }
+        public static Size Ceiling(SizeF value)
+        {
+            return new Size((int)Math.Ceiling(value.Width), (int)Math.Ceiling(value.Height));
+        }
 
+        public override bool Equals(Object obj)
+        {
+            if (obj is Size)
+                return (Size)obj == this;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static Size Round(SizeF value)
+        {
+            return new Size((int)Math.Round(value.Width), (int)Math.Round(value.Height));
+        }
+
+        public static Size Subtract(Size sz1, Size sz2)
+        {
+            return sz1 - sz2;
+        }
+
+        public override string ToString()
+        {
+            return $"({_width},{_height})";
+        }
+
+        public static Size Truncate(SizeF value)
+        {
+            return new Size((int)(value.Width), (int)(value.Height));
+        }
+
+        public static Size operator +(Size sz1, Size sz2)
+        {
+            return new Size(sz1.Width + sz2.Width, sz1.Height + sz2.Height);
+        }
+
+        public static bool operator ==(Size sz1, Size sz2)
+        {
+            return sz1.Width == sz2.Width && sz1.Height == sz2.Height;
+        }
+
+        public static explicit operator Point(Size size)
+        {
+            return new Point(size.Width, size.Height);
+        }
+
+        public static implicit operator SizeF(Size p)
+        {
+            return new SizeF(p.Width, p.Height);
+        }
+
+        public static bool operator !=(Size sz1, Size sz2)
+        {
+            return sz1.Width != sz2.Width || sz1.Height != sz2.Height;
+        }
+
+        public static Size operator -(Size sz1, Size sz2)
+        {
+            return new Size(sz1.Width - sz2.Width, sz1.Height - sz2.Height);
+        }
+        
         public static readonly Size Empty = new Size(0, 0);
     }
 }
