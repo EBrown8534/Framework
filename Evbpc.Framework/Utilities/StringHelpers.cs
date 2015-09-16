cf7e47468c30104faf2c1c06971ce42c257082ca
EBrown8534 <ebrown8534@gmail.com>
2015-09-16 12:16:32 -0400

    Removed (useless) comments.

--- Evbpc.Framework/Utilities/StringHelpers.cs
+++ Evbpc.Framework/Utilities/StringHelpers.cs
@@ -19,51 +19,45 @@ namespace Evbpc.Framework.Utilities
         /// <returns>The input byte-array encoded into a Base64 string, following the provided options.</returns>
         public static string ToBase64String(byte[] input, Base64FormattingOptions options = Base64FormattingOptions.RequirePaddingCharacter)
         {
-            // Setup the alphabets
             string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
             if ((options & Base64FormattingOptions.UrlFilenameSafeAlphabet) == Base64FormattingOptions.UrlFilenameSafeAlphabet)
                 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_=";
             else if ((options & Base64FormattingOptions.UnixCryptAlphabet) == Base64FormattingOptions.UnixCryptAlphabet)
                 alphabet = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz=";
-
-            // Create the result
+            
             string workingResult = "";
-
-            // Create our working set
-            int os = input.Length;
-            int ns = os;
+            
+            int originalLength = input.Length;
+            int newLength = originalLength;
 
             if (input.Length % 3 != 0)
-                ns += 3 - (os % 3);
+                newLength += 3 - (originalLength % 3);
 
-            byte[] workingSet = new byte[ns];
+            byte[] workingSet = new byte[newLength];
 
-            for (int i = 0; i < os; i++)
+            for (int i = 0; i < originalLength; i++)
             {
                 workingSet[i] = input[i];
             }
-
-            // Compute the Base64 string
-            for (int g = 0; g < ns / 3; g++)
+            
+            for (int g = 0; g < newLength / 3; g++)
             {
                 workingResult += alphabet[((workingSet[(g * 3)] & 0xFC) >> 2)];
                 workingResult += alphabet[((workingSet[(g * 3)] & 0x03) << 4) | ((workingSet[(g * 3) + 1] & 0xF0) >> 4)];
                 workingResult += alphabet[((workingSet[(g * 3) + 1] & 0x0F) << 2) | ((workingSet[(g * 3) + 2] & 0xC0) >> 6)];
                 workingResult += alphabet[((workingSet[(g * 3) + 2] & 0x3F))];
             }
-
-            // Add padding (If required)
+            
             if ((options & Base64FormattingOptions.RequirePaddingCharacter) == Base64FormattingOptions.RequirePaddingCharacter)
             {
-                if (os != ns)
+                if (originalLength != newLength)
                 {
-                    for (int p = 0; p < ns - os; p++)
+                    for (int p = 0; p < newLength - originalLength; p++)
                         workingResult += alphabet[64];
                 }
             }
-
-            // Break lines (If required)
+            
             int lineBreaks = 0;
 
             if ((options & Base64FormattingOptions.BreakLinesAt64Characters) == Base64FormattingOptions.BreakLinesAt64Characters)
@@ -80,8 +74,7 @@ namespace Evbpc.Framework.Utilities
             }
             else
                 result = workingResult;
-
-            // Return result
+            
             return result;
         }
 
@@ -93,7 +86,6 @@ namespace Evbpc.Framework.Utilities
         /// <returns>The input byte-array encoded into a Base32 string, following the provided options.</returns>
         public static string ToBase32String(this byte[] input, Base32FormattingOptions options = Base32FormattingOptions.RequirePaddingCharacter)
         {
-            // Setup the alphabets
             string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567=";
 
             if ((options & Base32FormattingOptions.CrockfordAlphabet) == Base32FormattingOptions.CrockfordAlphabet)
@@ -102,26 +94,23 @@ namespace Evbpc.Framework.Utilities
                 alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUV=";
             else if ((options & Base32FormattingOptions.VowelSafeAlphabet) == Base32FormattingOptions.VowelSafeAlphabet)
                 alphabet = "0123456789bcdfhjkmnpqrtvxyDFGHJL=";
-
-            // Create the result
+            
             string workingResult = "";
 
-            // Create our working set
-            int os = input.Length;
-            int ns = os;
+            int originalLength = input.Length;
+            int newLength = originalLength;
 
             if (input.Length % 5 != 0)
-                ns += os % 5;
+                newLength += originalLength % 5;
 
-            byte[] workingSet = new byte[ns];
+            byte[] workingSet = new byte[newLength];
 
-            for (int i = 0; i < os; i++)
+            for (int i = 0; i < originalLength; i++)
             {
                 workingSet[i] = input[i];
             }
-
-            // Compute the Base32 string
-            for (int g = 0; g < ns / 5; g++)
+            
+            for (int g = 0; g < newLength / 5; g++)
             {
                 workingResult += alphabet[((workingSet[(g * 5)] & 0xF4) >> 3)];
                 workingResult += alphabet[((workingSet[(g * 5)] & 0x03) << 2) | ((workingSet[(g * 5) + 1] & 0xC0) >> 6)];
@@ -132,13 +121,12 @@ namespace Evbpc.Framework.Utilities
                 workingResult += alphabet[((workingSet[(g * 5) + 3] & 0x03) << 3) | ((workingSet[(g * 5) + 4] & 0xE0) >> 5)];
                 workingResult += alphabet[((workingSet[(g * 5) + 4] & 0x1F))];
             }
-
-            // Add padding (If required)
+            
             if ((options & Base32FormattingOptions.RequirePaddingCharacter) == Base32FormattingOptions.RequirePaddingCharacter)
             {
-                if (os != ns)
+                if (originalLength != newLength)
                 {
-                    for (int p = 0; p < ns - os; p++)
+                    for (int p = 0; p < newLength - originalLength; p++)
                         workingResult += alphabet[32];
                 }
             }
@@ -146,8 +134,7 @@ namespace Evbpc.Framework.Utilities
             string result = "";
 
             result = workingResult;
-
-            // Return result
+            
             return result;
         }
 
@@ -159,39 +146,35 @@ namespace Evbpc.Framework.Utilities
         /// <returns>The input Base64 string decoded into a byte-array string, following the provided options.</returns>
         public static byte[] FromBase64String(string input, Base64FormattingOptions options = Base64FormattingOptions.RequirePaddingCharacter)
         {
-            // Setup the alphabets
             string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
             if ((options & Base64FormattingOptions.UrlFilenameSafeAlphabet) == Base64FormattingOptions.UrlFilenameSafeAlphabet)
                 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_=";
             else if ((options & Base64FormattingOptions.UnixCryptAlphabet) == Base64FormattingOptions.UnixCryptAlphabet)
                 alphabet = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz=";
-
-            // Merge lines
+            
             string workingSet = input.Replace("\r\n", "");
 
-            int os = input.Length;
-            int ns = os;
+            int originalLength = input.Length;
+            int newLength = originalLength;
 
-            if (ns % 4 != 0)
+            if (newLength % 4 != 0)
                 if ((options & Base64FormattingOptions.RequirePaddingCharacter) == Base64FormattingOptions.RequirePaddingCharacter)
                     throw new ArgumentException("The input string did not contain a required padding character.");
 
-            ns = ns / 4 * 3;
+            newLength = newLength / 4 * 3;
 
-            if (input[os - 1] == alphabet[64])
+            if (input[originalLength - 1] == alphabet[64])
             {
-                if (input[os - 2] == alphabet[64])
-                    ns -= 2;
+                if (input[originalLength - 2] == alphabet[64])
+                    newLength -= 2;
                 else
-                    ns -= 1;
+                    newLength -= 1;
             }
-
-            // Create the result
-            byte[] workingResult = new byte[ns];
-
-            // Decode the Base64 string
-            for (int g = 0; g < ns / 3; g++)
+            
+            byte[] workingResult = new byte[newLength];
+            
+            for (int g = 0; g < newLength / 3; g++)
             {
                 workingResult[g * 3] = (byte)(((alphabet.IndexOf(workingSet[g * 4])) << 2) | ((alphabet.IndexOf(workingSet[g * 4 + 1])) >> 4));
                 workingResult[g * 3 + 1] = (byte)(((alphabet.IndexOf(workingSet[g * 4 + 1])) << 4) | ((alphabet.IndexOf(workingSet[g * 4 + 2])) >> 2));
@@ -199,8 +182,7 @@ namespace Evbpc.Framework.Utilities
             }
 
             byte[] result = workingResult;
-
-            // Return result
+            
             return result;
         }
     }
