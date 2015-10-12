975aac32ea1920e1e2dc0b650633015d0158bd8b
EBrown8534 <ebrown8534@gmail.com>
2015-10-12 11:59:25 -0400

    Fixed XML docs on `ConsolePrompt`.

--- Evbpc.Framework/Utilities/Prompting/ConsolePrompt.cs
+++ Evbpc.Framework/Utilities/Prompting/ConsolePrompt.cs
@@ -21,7 +21,7 @@ namespace Evbpc.Framework.Utilities.Prompting
         /// </summary>
         /// <typeparam name="T">The type of input that should be returned.</typeparam>
         /// <param name="message">The message to initally display to the user.</param>
-        /// <param name="requireValue">Whether or not to allow use of a `defaultValue`.</param>
+        /// <param name="options">The <see cref="PromptOptions"/> that specify how the prompt should be treated.</param>
         /// <param name="defaultValue">The default value to be returned if a user enters an empty line (`""`).</param>
         /// <param name="failureMessage">An optional message to display on a failure. If null, then the `message` parameter will be displayed on failure.</param>
         /// <param name="validationMethod">An optional delegate to a method which can perform additional validation if the input is of the target type.</param>
