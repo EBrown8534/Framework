25ed990e2dd68b73b66b357a16bd426eb2c974bc
EBrown8534 <ebrown8534@gmail.com>
2015-10-23 12:27:35 -0400

    Added some unit tests for `NumberByteArrayExtensions` and made the methods in that class more DRY.

---
+++ Evbpc.Framework.Tests/Utilities/Extensions/NumberByteArrayExtensionsTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Evbpc.Framework.Utilities.Extensions.NumberByteArrayExtensions;
+
+namespace Evbpc.Framework.Tests.Utilities.Extensions
+{
+    [TestClass]
+    public class NumberByteArrayExtensionsTests
+    {
+        [TestMethod, TestCategory("Number Byte-Array Extensions Tests")]
+        public void UIntToByteArray_0xFF007FBF()
+        {
+            var sourceNumber = 0xFF007FBFu;
+            var resultArray = sourceNumber.ToByteArray();
+            var expectedResult = new byte[] { 0xFF, 0x00, 0x7F, 0xBF };
+
+            CollectionAssert.AreEqual(expectedResult, resultArray);
+        }
+
+        [TestMethod, TestCategory("Number Byte-Array Extensions Tests")]
+        public void ByteArrayToUInt_0xFF_0x00_0x7F_0xBF()
+        {
+            var sourceArray = new byte[] { 0xFF, 0x00, 0x7F, 0xBF };
+            var resultNumber = sourceArray.ToUInt32();
+            var expectedNumber = 0xFF007FBFu;
+
+            Assert.AreEqual(expectedNumber, resultNumber);
+        }
+
+        [TestMethod, TestCategory("Number Byte-Array Extensions Tests")]
+        public void ULongToByteArray_0xFFAF0FCF4F007FBF()
+        {
+            var sourceNumber = 0xFFAF0FCF4F007FBFu;
+            var resultArray = sourceNumber.ToByteArray();
+            var expectedResult = new byte[] { 0xFF, 0xAF, 0x0F, 0xCF, 0x4F, 0x00, 0x7F, 0xBF };
+
+            CollectionAssert.AreEqual(expectedResult, resultArray);
+        }
+
+        [TestMethod, TestCategory("Number Byte-Array Extensions Tests")]
+        public void ByteArrayToULong_0xFF_0xAF_0x0F_0xCF_0x4F_0x00_0x7F_0xBF()
+        {
+            var sourceArray = new byte[] { 0xFF, 0xAF, 0x0F, 0xCF, 0x4F, 0x00, 0x7F, 0xBF };
+            var resultNumber = sourceArray.ToUInt64();
+            var expectedNumber = 0xFFAF0FCF4F007FBFu;
+
+            Assert.AreEqual(expectedNumber, resultNumber);
+        }
+
+        [TestMethod, TestCategory("Number Byte-Array Extensions Tests")]
+        public void IntToByteArray_0x7F00FFBF()
+        {
+            var sourceNumber = 0x7F00FFBF;
+            var resultArray = sourceNumber.ToByteArray();
+            var expectedResult = new byte[] { 0x7F, 0x00, 0xFF, 0xBF };
+
+            CollectionAssert.AreEqual(expectedResult, resultArray);
+        }
+
+        [TestMethod, TestCategory("Number Byte-Array Extensions Tests")]
+        public void ByteArrayToInt_0x7F_0x00_0xFF_0xBF()
+        {
+            var sourceArray = new byte[] { 0x7F, 0x00, 0xFF, 0xBF };
+            var resultNumber = sourceArray.ToInt32();
+            var expectedNumber = 0x7F00FFBF;
+
+            Assert.AreEqual(expectedNumber, resultNumber);
+        }
+
+        [TestMethod, TestCategory("Number Byte-Array Extensions Tests")]
+        public void LongToByteArray_0x7FAF0FCF4F00FFBF()
+        {
+            var sourceNumber = 0x7FAF0FCF4F00FFBF;
+            var resultArray = sourceNumber.ToByteArray();
+            var expectedResult = new byte[] { 0x7F, 0xAF, 0x0F, 0xCF, 0x4F, 0x00, 0xFF, 0xBF };
+
+            CollectionAssert.AreEqual(expectedResult, resultArray);
+        }
+
+        [TestMethod, TestCategory("Number Byte-Array Extensions Tests")]
+        public void ByteArrayToLong_0x7F_0xAF_0x0F_0xCF_0x4F_0x00_0xFF_0xBF()
+        {
+            var sourceArray = new byte[] { 0x7F, 0xAF, 0x0F, 0xCF, 0x4F, 0x00, 0xFF, 0xBF };
+            var resultNumber = sourceArray.ToInt64();
+            var expectedNumber = 0x7FAF0FCF4F00FFBF;
+
+            Assert.AreEqual(expectedNumber, resultNumber);
+        }
+    }
+}
--- Evbpc.Framework/Utilities/Extensions/NumberByteArrayExtensions.cs
+++ Evbpc.Framework/Utilities/Extensions/NumberByteArrayExtensions.cs
@@ -17,14 +17,17 @@ namespace Evbpc.Framework.Utilities.Extensions
         /// <returns>A <code>byte[]</code> representing the <code>uint</code> value.</returns>
         public static byte[] ToByteArray(this uint value)
         {
-            byte[] result = new byte[4];
+            var size = 4;
+
+            byte[] result = new byte[size];
 
             uint t = value;
 
-            result[0] = (byte)((t & 0xFF000000u) >> 24);
-            result[1] = (byte)((t & 0x00FF0000u) >> 16);
-            result[2] = (byte)((t & 0x0000FF00u) >> 8);
-            result[3] = (byte)((t & 0x000000FFu) >> 0);
+            for (int i = 0; i < size; i++)
+            {
+                var bitOffset = (size - (i + 1)) * 8;
+                result[i] = (byte)((value & ((ulong)0xFF << bitOffset)) >> bitOffset);
+            }
 
             return result;
         }
@@ -36,14 +39,19 @@ namespace Evbpc.Framework.Utilities.Extensions
         /// <returns>A <code>uint</code> that represents the converted <code>byte[]</code>.</returns>
         public static uint ToUInt32(this byte[] data)
         {
-            if (data.Length != 4)
+            var requiredSize = 4;
+
+            if (data.Length != requiredSize)
             {
-                throw new ArgumentException($"The byte-array \"{nameof(data)}\" must be exactly four bytes.");
+                throw new ArgumentException($"The byte-array \"{nameof(data)}\" must be exactly {requiredSize} bytes.");
             }
 
             uint result = 0;
 
-            result = ((uint)data[0] << 24) | ((uint)data[1] << 16) | ((uint)data[2] << 8) | ((uint)data[3] << 0);
+            for (int i = 0; i < requiredSize; i++)
+            {
+                result |= ((uint)data[i] << ((requiredSize - (i + 1)) * 8));
+            }
 
             return result;
         }
@@ -55,16 +63,9 @@ namespace Evbpc.Framework.Utilities.Extensions
         /// <returns>A <code>byte[]</code> representing the <code>int</code> value.</returns>
         public static byte[] ToByteArray(this int value)
         {
-            byte[] result = new byte[4];
-
             uint t = (uint)value;
 
-            result[0] = (byte)((t & 0xFF000000u) >> 24);
-            result[1] = (byte)((t & 0x00FF0000u) >> 16);
-            result[2] = (byte)((t & 0x0000FF00u) >> 8);
-            result[3] = (byte)((t & 0x000000FFu) >> 0);
-
-            return result;
+            return t.ToByteArray();
         }
 
         /// <summary>
@@ -74,14 +75,16 @@ namespace Evbpc.Framework.Utilities.Extensions
         /// <returns>An <code>int</code> value representing the <code>byte[]</code>.</returns>
         public static int ToInt32(this byte[] data)
         {
-            if (data.Length != 4)
+            var requiredSize = 4;
+
+            if (data.Length != requiredSize)
             {
-                throw new ArgumentException($"The byte-array \"{nameof(data)}\" must be exactly four bytes.");
+                throw new ArgumentException($"The byte-array \"{nameof(data)}\" must be exactly {requiredSize} bytes.");
             }
 
             int result = 0;
 
-            result = ((int)data[0] << 24) | ((int)data[1] << 16) | ((int)data[2] << 8) | ((int)data[3] << 0);
+            result = (int)data.ToUInt32();
 
             return result;
         }
@@ -93,18 +96,15 @@ namespace Evbpc.Framework.Utilities.Extensions
         /// <returns>A <code>byte[]</code> representing the <code>ulong</code>.</returns>
         public static byte[] ToByteArray(this ulong value)
         {
-            byte[] result = new byte[8];
+            var size = 8;
 
-            ulong t = value;
+            byte[] result = new byte[size];
 
-            result[0] = (byte)((t & 0xFF00000000000000u) >> 56);
-            result[1] = (byte)((t & 0x00FF000000000000u) >> 48);
-            result[2] = (byte)((t & 0x0000FF0000000000u) >> 40);
-            result[3] = (byte)((t & 0x000000FF00000000u) >> 32);
-            result[4] = (byte)((t & 0x00000000FF000000u) >> 24);
-            result[5] = (byte)((t & 0x0000000000FF0000u) >> 16);
-            result[6] = (byte)((t & 0x000000000000FF00u) >> 8);
-            result[7] = (byte)((t & 0x00000000000000FFu) >> 0);
+            for (int i = 0; i < size; i++)
+            {
+                var bitOffset = (size - (i + 1)) * 8;
+                result[i] = (byte)((value & ((ulong)0xFF << bitOffset)) >> bitOffset);
+            }
 
             return result;
         }
@@ -116,14 +116,19 @@ namespace Evbpc.Framework.Utilities.Extensions
         /// <returns>A <code>ulong</code> reprented by the <code>byte[]</code>.</returns>
         public static ulong ToUInt64(this byte[] data)
         {
-            if (data.Length != 8)
+            var requiredSize = 8;
+
+            if (data.Length != requiredSize)
             {
-                throw new ArgumentException($"The byte-array \"{nameof(data)}\" must be exactly eight bytes.");
+                throw new ArgumentException($"The byte-array \"{nameof(data)}\" must be exactly {requiredSize} bytes.");
             }
 
             ulong result = 0;
 
-            result = ((ulong)data[0] << 56) | ((ulong)data[1] << 48) | ((ulong)data[2] << 40) | ((ulong)data[3] << 32) | ((ulong)data[4] << 24) | ((ulong)data[5] << 16) | ((ulong)data[6] << 8) | ((ulong)data[7] << 0);
+            for (int i = 0; i < requiredSize; i++)
+            {
+                result |= ((ulong)data[i] << ((requiredSize - (i + 1)) * 8));
+            }
 
             return result;
         }
@@ -135,20 +140,9 @@ namespace Evbpc.Framework.Utilities.Extensions
         /// <returns>A <code>byte[]</code> representing the <code>long</code> value.</returns>
         public static byte[] ToByteArray(this long value)
         {
-            byte[] result = new byte[8];
-
             ulong t = (ulong)value;
 
-            result[0] = (byte)((t & 0xFF00000000000000u) >> 56);
-            result[1] = (byte)((t & 0x00FF000000000000u) >> 48);
-            result[2] = (byte)((t & 0x0000FF0000000000u) >> 40);
-            result[3] = (byte)((t & 0x000000FF00000000u) >> 32);
-            result[4] = (byte)((t & 0x00000000FF000000u) >> 24);
-            result[5] = (byte)((t & 0x0000000000FF0000u) >> 16);
-            result[6] = (byte)((t & 0x000000000000FF00u) >> 8);
-            result[7] = (byte)((t & 0x00000000000000FFu) >> 0);
-
-            return result;
+            return t.ToByteArray();
         }
 
         /// <summary>
@@ -158,14 +152,16 @@ namespace Evbpc.Framework.Utilities.Extensions
         /// <returns>A <code>long</code> value represented by the <code>byte[]</code>.</returns>
         public static long ToInt64(this byte[] data)
         {
-            if (data.Length != 8)
+            var requiredSize = 8;
+
+            if (data.Length != requiredSize)
             {
-                throw new ArgumentException($"The byte-array \"{nameof(data)}\" must be exactly eight bytes.");
+                throw new ArgumentException($"The byte-array \"{nameof(data)}\" must be exactly {requiredSize} bytes.");
             }
 
             long result = 0;
 
-            result = ((long)data[0] << 56) | ((long)data[1] << 48) | ((long)data[2] << 40) | ((long)data[3] << 32) | ((long)data[4] << 24) | ((long)data[5] << 16) | ((long)data[6] << 8) | ((long)data[7] << 0);
+            result = (long)data.ToUInt64();
 
             return result;
         }
