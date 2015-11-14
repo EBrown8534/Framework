42cec8c3fd50439e3aefb6b990c6b4e0bba1b2e8
EBrown8534 <ebrown8534@gmail.com>
2015-11-14 11:17:22 -0500

    Changed a little bit of the `AesCrypto` implementation, and made it use `Rijndael` isntead of `Aes` for the extra security.

--- Evbpc.Framework/Utilities/Cryptography/AesCrypto.cs
+++ Evbpc.Framework/Utilities/Cryptography/AesCrypto.cs
@@ -11,7 +11,7 @@ namespace Evbpc.Framework.Utilities.Cryptography
     /// <summary>
     /// Provides Cryptography methods based on AES cryptography implementation.
     /// </summary>
-    public class AesCrypto
+    public class AesCrypto : IDisposable
     {
         /// <summary>
         /// Gets or sets the salt to use with AES encryption.
@@ -22,6 +22,11 @@ namespace Evbpc.Framework.Utilities.Cryptography
         /// Gets or sets the passphrase for use with AES encryption.
         /// </summary>
         public string Passphrase { get; set; }
+
+        private Rijndael _encryptor;
+        private Rfc2898DeriveBytes _pdb;
+        private ICryptoTransform _encryptorTransform;
+        private ICryptoTransform _decryptorTransform;
 
         /// <summary>
         /// Constructs a new instance of <see cref="AesCrypto"/> from the specified values.
@@ -35,13 +40,21 @@ namespace Evbpc.Framework.Utilities.Cryptography
                 throw new ArgumentException($"The parameter {nameof(passphrase)} is required.");
             }
 
-            if (salt == null || salt.Length == 0)
+            if (salt == null || salt.Length < 8)
             {
-                throw new ArgumentException($"The parameter {nameof(salt)} is required.");
+                throw new ArgumentException($"The parameter {nameof(salt)} is required, and must be at least 8 bytes.");
             }
 
             Salt = salt;
             Passphrase = passphrase;
+
+            _encryptor = Rijndael.Create();
+            _pdb = new Rfc2898DeriveBytes(Passphrase, Salt);
+            _encryptor.Key = _pdb.GetBytes(32);
+            _encryptor.IV = _pdb.GetBytes(16);
+
+            _encryptorTransform = _encryptor.CreateEncryptor();
+            _decryptorTransform = _encryptor.CreateDecryptor();
         }
 
         /// <summary>
@@ -51,80 +64,84 @@ namespace Evbpc.Framework.Utilities.Cryptography
         /// <returns>An encrypted Base64 string.</returns>
         public string AesEncrypt(string clearText)
         {
-            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            var clearBytes = Encoding.Unicode.GetBytes(clearText);
 
-            using (Aes encryptor = Aes.Create())
+            using (var ms = new MemoryStream())
             {
-                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(Passphrase, Salt);
-                encryptor.Key = pdb.GetBytes(32);
-                encryptor.IV = pdb.GetBytes(16);
-
-                using (MemoryStream ms = new MemoryStream())
+                using (var cs = new CryptoStream(ms, _encryptorTransform, CryptoStreamMode.Write))
                 {
-                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
-                    {
-                        cs.Write(clearBytes, 0, clearBytes.Length);
-                        cs.Close();
-                    }
-
-                    clearText = StringExtensions.ToBase64String(ms.ToArray());
+                    cs.Write(clearBytes, 0, clearBytes.Length);
                 }
-            }
 
-            return clearText;
+                return StringExtensions.ToBase64String(ms.GetBuffer());
+            }
         }
 
         /// <summary>
         /// Uses AES encryption to decrypt a string of data.
         /// </summary>
         /// <param name="cipherText">The encrypted Base64 string to decrypt.</param>
-        /// <param name="Passphrase">The secret pre-shared passphrase.</param>
         /// <param name="throwExceptions">If true, will throw exceptions on decryption failure. Else, returns null string on decryption failure.</param>
         /// <returns>The plaintext data. Data is unicode.</returns>
         public string AesDecrypt(string cipherText, bool throwExceptions)
         {
-            if (throwExceptions)
+            try
             {
-                cipherText = AesDecryptInternal(cipherText);
-            }
-            else
-            {
-                try
+                var cipherBytes = StringExtensions.FromBase64String(cipherText);
+
+                using (var ms = new MemoryStream())
                 {
-                    cipherText = AesDecryptInternal(cipherText);
-                }
-                catch
-                {
-                    cipherText = null;
-                }
-            }
-
-            return cipherText;
-        }
-
-        private string AesDecryptInternal(string cipherText)
-        {
-            byte[] cipherBytes = StringExtensions.FromBase64String(cipherText);
-
-            using (Aes encryptor = Aes.Create())
-            {
-                Rfc2898DeriveBytes saltDerived = new Rfc2898DeriveBytes(Passphrase, Salt);
-                encryptor.Key = saltDerived.GetBytes(32);
-                encryptor.IV = saltDerived.GetBytes(16);
-
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    using (var cs = new CryptoStream(ms, _decryptorTransform, CryptoStreamMode.Write))
                     {
                         cs.Write(cipherBytes, 0, cipherBytes.Length);
-                        cs.Close();
                     }
 
-                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                    return Encoding.Unicode.GetString(ms.GetBuffer());
                 }
             }
-
-            return cipherText;
+            catch when (!throwExceptions)
+            {
+                return null;
+            }
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                    _encryptor.Dispose();
+                    _encryptorTransform.Dispose();
+                    _decryptorTransform.Dispose();
+                    _pdb.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~AesCrypto() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
