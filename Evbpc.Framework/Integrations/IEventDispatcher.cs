7b59e0a3e4717a8e430f2f860fc1abc82ab15e7b
EBrown8534 <ebrown8534@gmail.com>
2015-12-03 18:31:01 -0500

    Removed `Deserialize` method from `IEventDispatcher`.

--- Evbpc.Framework/Integrations/IEventDispatcher.cs
+++ Evbpc.Framework/Integrations/IEventDispatcher.cs
@@ -6,14 +6,6 @@
     public interface IEventDispatcher
     {
         /// <summary>
-        /// Deserializes a type with a <code>DataContractJsonSerializer</code>.
-        /// </summary>
-        /// <typeparam name="T">The type that should be deserialized.</typeparam>
-        /// <param name="json">The JSON to deserialize.</param>
-        /// <returns>The deserialized object.</returns>
-        T Deserialze<T>(string json);
-
-        /// <summary>
         /// Dispatches an event corresponding to the specified eventKey and json data.
         /// </summary>
         /// <param name="eventKey">The event key of the event.</param>
