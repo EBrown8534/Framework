fdb2614be200ad7695972475833a9c818a945f9d
EBrown8534 <ebrown8534@gmail.com>
2015-12-04 10:30:49 -0500

    Added a `DataContractJsonSerialization` helper class.

---
+++ Evbpc.Framework/Utilities/Serialization/DataContractJsonSerialization.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evbpc.Framework.Utilities.Serialization
+{
+    /// <summary>
+    /// Helper methods for serialization with <code>DataContractSerializer</code>.
+    /// </summary>
+    public static class DataContractJsonSerialization
+    {
+        /// <summary>
+        /// Deserializes a JSON string using a <code>DataContractSerializer</code>.
+        /// </summary>
+        /// <typeparam name="T">The type to deserialize to.</typeparam>
+        /// <param name="json">The JSON to deserialize.</param>
+        /// <returns>The deserialized JSON to the specified type.</returns>
+        public static T Deserialze<T>(string json)
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            {
+                ms.Position = 0;
+                return (T)serializer.ReadObject(ms);
+            }
+        }
+    }
+}
