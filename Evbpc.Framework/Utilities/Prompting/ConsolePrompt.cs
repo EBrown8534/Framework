9936e3590e9934806a8ac103721928a75d2d80cb
EBrown8534 <ebrown8534@gmail.com>
2016-01-08 18:16:53 -0500

    Added a `IPrompt.Prompt` method which works like a `TryPrompt` style of method.

--- Evbpc.Framework/Utilities/Prompting/ConsolePrompt.cs
+++ Evbpc.Framework/Utilities/Prompting/ConsolePrompt.cs
@@ -45,6 +45,44 @@ namespace Evbpc.Framework.Utilities.Prompting
                            Func<T, T> parseResultMethod = null)
             where T : IConvertible
         {
+            T result = defaultValue;
+            Prompt(out result, message, options, defaultValue, failureMessage, validationMethod, parseResultMethod, 0);
+            return result;
+        }
+
+        private static bool Retrieve<T>(string line, out T resultValue)
+            where T : IConvertible
+        {
+            var type = typeof(T);
+
+            resultValue = default(T);
+
+            try
+            {
+                resultValue = (T)Convert.ChangeType(line, type);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Prompts the user for input.
+        /// </summary>
+        /// <typeparam name="T">The type of input that should be returned.</typeparam>
+        /// <param name="result">Filled with the result of collecting the input from the user and applying it to the specified type.</param>
+        /// <param name="message">The message to initally display to the user.</param>
+        /// <param name="options">The <see cref="PromptOptions"/> that specify how the prompt should be treated.</param>
+        /// <param name="defaultValue">The default value to be returned if a user enters an empty line (`""`).</param>
+        /// <param name="failureMessage">An optional message to display on a failure. If null, then the `message` parameter will be displayed on failure.</param>
+        /// <param name="validationMethod">An optional delegate to a method which can perform additional validation if the input is of the target type.</param>
+        /// <param name="parseResultMethod">An optional delegate to a method which will parse the final result (assuming validity checks all pass).</param>
+        /// <param name="maxTries">An optional integer specifying the maximum number of attempts to make. If <code>0</code> or smaller then the number of attempts will not be limited.</param>
+        /// <returns>A boolean value indicating success. If <code>true</code> then result is filled with the user specified input, or the default value. If <code>false</code> then result will be <code>default(T)</code>.</returns>
+        public bool Prompt<T>(out T result, string message, PromptOptions options, T defaultValue = default(T), string failureMessage = null, Func<T, bool> validationMethod = null, Func<T, T> parseResultMethod = null, int maxTries = 0) where T : IConvertible
+        {
             Console.Write(message);
 
             if (options == PromptOptions.Optional)
@@ -55,7 +93,8 @@ namespace Evbpc.Framework.Utilities.Prompting
             Console.Write(": ");
 
             bool pass = false;
-            T result = default(T);
+            result = default(T);
+            int tries = 0;
 
             while (!pass)
             {
@@ -76,8 +115,16 @@ namespace Evbpc.Framework.Utilities.Prompting
                     result = defaultValue;
                 }
 
+                tries++;
+
                 if (pass)
                 {
+                    break;
+                }
+
+                if (tries == maxTries)
+                {
+                    pass = false;
                     break;
                 }
 
@@ -98,25 +145,7 @@ namespace Evbpc.Framework.Utilities.Prompting
                 result = parseResultMethod.Invoke(result);
             }
 
-            return result;
-        }
-
-        private static bool Retrieve<T>(string line, out T resultValue)
-            where T : IConvertible
-        {
-            var type = typeof(T);
-
-            resultValue = default(T);
-
-            try
-            {
-                resultValue = (T)Convert.ChangeType(line, type);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
+            return pass;
         }
     }
 }
--- Evbpc.Framework/Utilities/Prompting/IPrompt.cs
+++ Evbpc.Framework/Utilities/Prompting/IPrompt.cs
@@ -30,5 +30,21 @@ namespace Evbpc.Framework.Utilities.Prompting
         /// <returns>The input collected from the user when it is deemed valid.</returns>
         T Prompt<T>(string message, PromptOptions options, T defaultValue = default(T), string failureMessage = null, Func<T, bool> validationMethod = null, Func<T, T> parseResultMethod = null)
             where T : IConvertible;
+
+        /// <summary>
+        /// Prompts the user for input.
+        /// </summary>
+        /// <typeparam name="T">The type of input that should be returned.</typeparam>
+        /// <param name="result">Filled with the result of collecting the input from the user and applying it to the specified type.</param>
+        /// <param name="message">The message to initally display to the user.</param>
+        /// <param name="options">The <see cref="PromptOptions"/> that specify how the prompt should be treated.</param>
+        /// <param name="defaultValue">The default value to be returned if a user enters an empty line (`""`).</param>
+        /// <param name="failureMessage">An optional message to display on a failure. If null, then the `message` parameter will be displayed on failure.</param>
+        /// <param name="validationMethod">An optional delegate to a method which can perform additional validation if the input is of the target type.</param>
+        /// <param name="parseResultMethod">An optional delegate to a method which will parse the final result (assuming validity checks all pass).</param>
+        /// <param name="maxTries">An optional integer specifying the maximum number of attempts to make. If <code>0</code> or smaller then the number of attempts will not be limited.</param>
+        /// <returns>A boolean value indicating success. If <code>true</code> then result is filled with the user specified input, or the default value. If <code>false</code> then result will be <code>default(T)</code>.</returns>
+        bool Prompt<T>(out T result, string message, PromptOptions options, T defaultValue = default(T), string failureMessage = null, Func<T, bool> validationMethod = null, Func<T, T> parseResultMethod = null, int maxTries = 0)
+            where T : IConvertible;
     }
 }
