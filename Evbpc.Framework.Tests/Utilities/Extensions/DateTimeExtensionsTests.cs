beb556fb1b2fa758c321217366401d4289229724
EBrown8534 <ebrown8534@gmail.com>
2016-04-10 00:12:37 -0400

    Updated `DateTimeExtensions` and `DateTimeExtensionTests` to use `DateTimeKind.Utc`; updated `DateTimeExtensions` to use a private readonly field for the Unix Epoch base instead of creating an instance any time `ToEpoch` and `FromEpoch` are called. Tests still pass, life is good.

--- Evbpc.Framework.Tests/Utilities/Extensions/DateTimeExtensionsTests.cs
+++ Evbpc.Framework.Tests/Utilities/Extensions/DateTimeExtensionsTests.cs
@@ -14,7 +14,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         [TestMethod, TestCategory("Date Time Extensions Tests")]
         public void EpochToDateTime_0()
         {
-            var expected = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            var expected = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             var input = 0;
 
             var result = DateTimeExtensions.FromEpoch(input);
@@ -26,7 +26,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         public void DateTimeToEpoch_1970_1_1_0_0_0_0()
         {
             var expected = 0;
-            var input = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            var input = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 
             var result = DateTimeExtensions.ToEpoch(input);
 
@@ -36,7 +36,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         [TestMethod, TestCategory("Date Time Extensions Tests")]
         public void EpochToDateTime_1()
         {
-            var expected = new DateTime(1970, 1, 1, 0, 0, 1, 0);
+            var expected = new DateTime(1970, 1, 1, 0, 0, 1, 0, DateTimeKind.Utc);
             var input = 1;
 
             var result = DateTimeExtensions.FromEpoch(input);
@@ -48,7 +48,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         public void DateTimeToEpoch_1970_1_1_0_0_1_0()
         {
             var expected = 1;
-            var input = new DateTime(1970, 1, 1, 0, 0, 1, 0);
+            var input = new DateTime(1970, 1, 1, 0, 0, 1, 0, DateTimeKind.Utc);
 
             var result = DateTimeExtensions.ToEpoch(input);
 
@@ -58,7 +58,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         [TestMethod, TestCategory("Date Time Extensions Tests")]
         public void EpochToDateTime_61()
         {
-            var expected = new DateTime(1970, 1, 1, 0, 1, 1, 0);
+            var expected = new DateTime(1970, 1, 1, 0, 1, 1, 0, DateTimeKind.Utc);
             var input = 61;
 
             var result = DateTimeExtensions.FromEpoch(input);
@@ -70,7 +70,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         public void DateTimeToEpoch_1970_1_1_0_1_1_0()
         {
             var expected = 61;
-            var input = new DateTime(1970, 1, 1, 0, 1, 1, 0);
+            var input = new DateTime(1970, 1, 1, 0, 1, 1, 0, DateTimeKind.Utc);
 
             var result = DateTimeExtensions.ToEpoch(input);
 
@@ -80,7 +80,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         [TestMethod, TestCategory("Date Time Extensions Tests")]
         public void EpochToDateTime_3601()
         {
-            var expected = new DateTime(1970, 1, 1, 1, 0, 1, 0);
+            var expected = new DateTime(1970, 1, 1, 1, 0, 1, 0, DateTimeKind.Utc);
             var input = 3601;
 
             var result = DateTimeExtensions.FromEpoch(input);
@@ -92,7 +92,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         public void DateTimeToEpoch_1970_1_1_1_0_1_0()
         {
             var expected = 3601;
-            var input = new DateTime(1970, 1, 1, 1, 0, 1, 0);
+            var input = new DateTime(1970, 1, 1, 1, 0, 1, 0, DateTimeKind.Utc);
 
             var result = DateTimeExtensions.ToEpoch(input);
 
@@ -102,7 +102,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         [TestMethod, TestCategory("Date Time Extensions Tests")]
         public void EpochToDateTime_86401()
         {
-            var expected = new DateTime(1970, 1, 2, 0, 0, 1, 0);
+            var expected = new DateTime(1970, 1, 2, 0, 0, 1, 0, DateTimeKind.Utc);
             var input = 86401;
 
             var result = DateTimeExtensions.FromEpoch(input);
@@ -114,7 +114,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         public void DateTimeToEpoch_1970_1_2_0_0_1_0()
         {
             var expected = 86401;
-            var input = new DateTime(1970, 1, 2, 0, 0, 1, 0);
+            var input = new DateTime(1970, 1, 2, 0, 0, 1, 0, DateTimeKind.Utc);
 
             var result = DateTimeExtensions.ToEpoch(input);
 
@@ -124,7 +124,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         [TestMethod, TestCategory("Date Time Extensions Tests")]
         public void EpochToDateTime_2678401()
         {
-            var expected = new DateTime(1970, 2, 1, 0, 0, 1, 0);
+            var expected = new DateTime(1970, 2, 1, 0, 0, 1, 0, DateTimeKind.Utc);
             var input = 2678401;
 
             var result = DateTimeExtensions.FromEpoch(input);
@@ -136,7 +136,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         public void DateTimeToEpoch_1970_2_1_0_0_1_0()
         {
             var expected = 2678401;
-            var input = new DateTime(1970, 2, 1, 0, 0, 1, 0);
+            var input = new DateTime(1970, 2, 1, 0, 0, 1, 0, DateTimeKind.Utc);
 
             var result = DateTimeExtensions.ToEpoch(input);
 
@@ -146,7 +146,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         [TestMethod, TestCategory("Date Time Extensions Tests")]
         public void EpochToDateTime_31536001()
         {
-            var expected = new DateTime(1971, 1, 1, 0, 0, 1, 0);
+            var expected = new DateTime(1971, 1, 1, 0, 0, 1, 0, DateTimeKind.Utc);
             var input = 31536001;
 
             var result = DateTimeExtensions.FromEpoch(input);
@@ -158,7 +158,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         public void DateTimeToEpoch_1971_1_1_0_0_1_0()
         {
             var expected = 31536001;
-            var input = new DateTime(1971, 1, 1, 0, 0, 1, 0);
+            var input = new DateTime(1971, 1, 1, 0, 0, 1, 0, DateTimeKind.Utc);
 
             var result = DateTimeExtensions.ToEpoch(input);
 
@@ -168,7 +168,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         [TestMethod, TestCategory("Date Time Extensions Tests")]
         public void EpochToDateTime_315532801()
         {
-            var expected = new DateTime(1980, 1, 1, 0, 0, 1, 0);
+            var expected = new DateTime(1980, 1, 1, 0, 0, 1, 0, DateTimeKind.Utc);
             var input = 315532801;
 
             var result = DateTimeExtensions.FromEpoch(input);
@@ -180,7 +180,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         public void DateTimeToEpoch_1980_1_1_0_0_1_0()
         {
             var expected = 315532801;
-            var input = new DateTime(1980, 1, 1, 0, 0, 1, 0);
+            var input = new DateTime(1980, 1, 1, 0, 0, 1, 0, DateTimeKind.Utc);
 
             var result = DateTimeExtensions.ToEpoch(input);
 
@@ -190,7 +190,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         [TestMethod, TestCategory("Date Time Extensions Tests")]
         public void EpochToDateTime_1420070401()
         {
-            var expected = new DateTime(2015, 1, 1, 0, 0, 1, 0);
+            var expected = new DateTime(2015, 1, 1, 0, 0, 1, 0, DateTimeKind.Utc);
             var input = 1420070401;
 
             var result = DateTimeExtensions.FromEpoch(input);
@@ -202,7 +202,7 @@ namespace Evbpc.Framework.Tests.Utilities.Extensions
         public void DateTimeToEpoch_2015_1_1_0_0_1_0()
         {
             var expected = 1420070401;
-            var input = new DateTime(2015, 1, 1, 0, 0, 1, 0);
+            var input = new DateTime(2015, 1, 1, 0, 0, 1, 0, DateTimeKind.Utc);
 
             var result = DateTimeExtensions.ToEpoch(input);
 
--- Evbpc.Framework/Utilities/Extensions/DateTimeExtensions.cs
+++ Evbpc.Framework/Utilities/Extensions/DateTimeExtensions.cs
@@ -11,6 +11,11 @@ namespace Evbpc.Framework.Utilities.Extensions
     public static class DateTimeExtensions
     {
         /// <summary>
+        /// Represents January 1, 1970 which is Unix Epoch time 0.
+        /// </summary>
+        public static readonly DateTime EpochBase = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+        /// <summary>
         /// Converts an <code>IList&lt;DateTime&gt;</code> to the <code>IList&lt;long&gt;</code> equivalent.
         /// </summary>
         /// <param name="dateList">An <code>IList&lt;DateTime&gt;</code> to be converted.</param>
@@ -29,14 +34,14 @@ namespace Evbpc.Framework.Utilities.Extensions
         /// </summary>
         /// <param name="dateTime">A DateTime to convert to epoch time.</param>
         /// <returns>The long number of seconds since the unix epoch.</returns>
-        public static long ToEpoch(DateTime dateTime) => (long)(dateTime - new DateTime(1970, 1, 1)).TotalSeconds;
+        public static long ToEpoch(DateTime dateTime) => (long)(dateTime - EpochBase).TotalSeconds;
 
         /// <summary>
         /// Converts a long representation of time since the unix epoch to a DateTime.
         /// </summary>
         /// <param name="epoch">The number of seconds since Jan 1, 1970.</param>
         /// <returns>A DateTime representing the time since the epoch.</returns>
-        public static DateTime FromEpoch(long epoch) => new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified).AddSeconds(epoch);
+        public static DateTime FromEpoch(long epoch) => EpochBase.AddSeconds(epoch);
 
         /// <summary>
         /// Converts a DateTime? to the long? representation which is the number of seconds since the unix epoch.
