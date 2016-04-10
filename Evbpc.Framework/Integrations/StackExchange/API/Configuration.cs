fdea2b0880596442b90afe9f17bc30153b4a7e60
EBrown8534 <ebrown8534@gmail.com>
2016-04-10 00:19:08 -0400

    Added a `Configuration` class for Stack Exchange API requests. This is a new technique (to me at least) that I'm going to try with this API to see how well it works.

---
+++ Evbpc.Framework/Integrations/StackExchange/API/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evbpc.Framework.Integrations.StackExchange.API
+{
+    /// <summary>
+    /// Represents a Stack Exchange API configuration for use with API requests.
+    /// </summary>
+    public class Configuration
+    {
+        /// <summary>
+        /// The application API key. Can be <code>null</code> for anonymous requests.
+        /// </summary>
+        public string Key { get; set; }
+    }
+}
